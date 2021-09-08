4f5e3ad1385589c9a90e19f2b923f583534e3b11
oluwadare <wintan1418@gmail.com>
2021-09-08 19:30:21 +0100

    add function for dispatching category method

--- src/component/Category.js
+++ src/component/Category.js
@@ -2,3 +2,12 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCategoriesStartAsync } from './actionCategories';
+
+const Categories = props => {
+  const Categories = useSelector(state => state.Categories.Categories);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategoriesStartAsync());
+  }, []);
+};
