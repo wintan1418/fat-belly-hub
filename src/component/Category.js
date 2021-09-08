4bbca3c02626568341dae74c3fb6f9fab4476575
oluwadare <wintan1418@gmail.com>
2021-09-08 19:24:32 +0100

    add import statements to category.js

---
+++ src/component/Category.js
@@ -0,0 +1,4 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCategoriesStartAsync } from './actionCategories';
