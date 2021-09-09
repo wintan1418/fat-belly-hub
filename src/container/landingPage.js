40b6f5c00c2aebe5cf2d704c2075930b8a3b8230
oluwadare <wintan1418@gmail.com>
2021-09-09 10:10:42 +0100

    add and finish logic for home.js

--- src/container/landingPage.js
+++ src/container/landingPage.js
@@ -1,6 +1,27 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import Categories from '../component/Category'
+import Categories from '../component/Category';
 import RecipeList from './Recipe';
-import {fetchIngredientsStartAsync} from '../actions/actionIngredient';
+import { fetchIngredientsStartAsync } from '../actions/actionIngredient';
 
+const Home = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [state, setState] = useState('Fish');
+  const dispatch = useDispatch();
+
+  const handleCategoryChange = category => {
+    setState(category);
+  };
+
+  useEffect(() => {
+    dispatch(fetchIngredientsStartAsync());
+  }, []);
+
+  return (
+    <div>
+      <Categories onCategoryChange={handleCategoryChange} />
+      <RecipeList handleChange={handleCategoryChange} />
+    </div>
+  );
+};
+export default Home;
