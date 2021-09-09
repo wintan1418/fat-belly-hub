adb4761cdc305d99686aa8a8a71925760479c0c9
oluwadare <wintan1418@gmail.com>
2021-09-09 18:35:58 +0100

    return container for outline

--- src/container/Recipe.js
+++ src/container/Recipe.js
@@ -1,18 +1,32 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  Grid,Image,Card,Container,
+  Grid, Image, Card, Container,
 } from 'semantic-ui-react';
 import { fetchMealsStartAsync } from '../actions/actionMeals';
 
 const RecipeList = props => {
-  const {cat } = props;
+  const { cat } = props;
   const meals = useSelector(state => state.meals.meals);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchMealsStartAsync(cat));
   }, [cat]);
-}
+
+return (
+  container textalign="center" style={{marginTop: 40}}>
+  <Grid columns={3}>
+    <Grid.Row>
+      {meals && meals.map(meal => (
+        <Grid.column
+      )
+
+      )}
+    </Grid.Row>
+  </Grid>
+)
+
+};
