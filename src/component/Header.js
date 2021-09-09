e17ae346fdbf212e42ea4c4558c142e69fb8d128
oluwadare <wintan1418@gmail.com>
2021-09-09 08:31:24 +0100

    finish header alignment

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,14 +1,25 @@
-import React, {usestate} from 'react';
-import {Menu} from 'semantic-ui-react';
+import React, { usestate } from 'react';
+import { Menu } from 'semantic-ui-react';
 import { link } from 'react-router-dom';
 
 const Header = () => {
   const { pathname } = window.location;
 
-  const path  = pathname === '/' ? 'home' : pathname.substr(1);
+  const path = pathname === '/' ? 'home' : pathname.substr(1);
 
   const [activeItem, setActiveItem] = useState(path);
 
-  const handleItemClick = (e, {name }) => setActiveItem(name);
-  
-}
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+  return (
+    <menu pointing secondary size="massive" color="orange" textalign="center">
+      <Menu.Item
+        name="Fat-Belly-Hub Api"
+        active={activeItem === 'home'}
+        onClick={handleItemClick}
+        as={link}
+        to="/"
+      />
+    </menu>
+  );
+};
