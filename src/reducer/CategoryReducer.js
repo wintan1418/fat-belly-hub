f9b847759a52a23bb41a1b1464d5a696ab8bba9d
oluwadare <wintan1418@gmail.com>
2021-09-10 09:44:33 +0100

    add functions and switch cases for fetch data

--- src/reducer/CategoryReducer.js
+++ src/reducer/CategoryReducer.js
@@ -1,8 +1,24 @@
-import { CategoriesActionTypes } from "../actions/actionType";
+import { CategoriesActionTypes } from '../actions/actionType';
+import Categories from '../component/Category';
 
 const INITIAL_STATE = {
   categories: [],
   isFetching: false,
-  errorMessage:undefined,
+  errorMessage: undefined,
+};
 
+const categoriesReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case CategoriesActionTypes.FETCH_DATA_BEGIN:
+      return {
+        ...state,
+        isFetching: true,
+      };
+  
+  case CategoriesActionTypes.FETCH_DATA_PROGRESS:
+    return{
+      ...state,
+      categories action.categories,
+      isFetching: false,
+    };
 };
