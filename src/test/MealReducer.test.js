1f929a6372301640e60d9f82a17f604bfb693cda
oluwadare <wintan1418@gmail.com>
2021-09-13 17:50:00 +0100

    Fix failing test

--- src/test/MealReducer.test.js
+++ src/test/MealReducer.test.js
@@ -1,20 +1,8 @@
 import mealsReducer from '../reducer/MealReducer';
 
 describe('Meals Reducer', () => {
-  it('1. Checks the default state is returned', () => {
-    const mockState = undefined;
-    const mockAction = { type: 'any' };
-    const mockFetching = false;
-    const state = mealsReducer(mockState, mockAction, mockFetching);
-    expect(state).toStrictEqual({
-      errorMessage: undefined,
-      meals: [],
-      isFetching: false,
-    });
-  });
-
   it(
-    '2. Checks the default action for the reducer, should return given state',
+    ' Checks the default action for the reducer, should return given state',
     () => {
       const mockState = {
         meals: [],
